b92b1e181757510632f8ffbbb5dfea58092d9b40
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-10-07 18:00:37 -0400

    forgot email link send successfully

---
+++ middleware/error.js
@@ -0,0 +1,12 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  //wrong Mongodb Id error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+};
