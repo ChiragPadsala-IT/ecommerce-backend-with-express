816c3462d2708dc999013d0643651162490c121c
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-06 02:26:03 -0500

    take token from header instead of cookie

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,7 +4,7 @@ const User = require("../model/userModel");
 const ErrorHandler = require("../utils/errorHandler");
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.headers.authorization.split(" ")[1];
 
   if (!token) {
     return next(new ErrorHandler("Please log in...", 401));
@@ -15,4 +15,4 @@ exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
   req.user = await User.findById(decodeData.id);
 
   next();
-}); 
+});
