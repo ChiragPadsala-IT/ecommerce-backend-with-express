7fc6216eb39aa27b94c917412c03d483fab02a4a
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-06 03:37:49 -0500

    nothing changed

--- routes/favoriteRoute.js
+++ routes/favoriteRoute.js
@@ -8,16 +8,16 @@ const { isAuthenticatedUser } = require("../middleware/isAuthenticated");
 
 const route = express.Router();
 
-// route
-//   .route("/add-to-favorite")
-//   .post(isAuthenticatedUser, addToFavoriteController);
+route
+  .route("/add-to-favorite")
+  .post(isAuthenticatedUser, addToFavoriteController);
 
 route
   .route("/favorite-product")
   .get(isAuthenticatedUser, getFavoriteController);
 
-// route
-//   .route("/remove-product")
-//   .post(isAuthenticatedUser, removeFromFavoriteController);
+route
+  .route("/remove-product")
+  .post(isAuthenticatedUser, removeFromFavoriteController);
 
 module.exports = route;
