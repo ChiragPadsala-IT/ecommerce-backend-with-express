4a7dbe40a8760d7d6ae5b8921a1cfcef7d08f693
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-07 05:42:06 -0500

    add payment gateway

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,9 +2,11 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 const Product = require("../model/productModel");
 const Order = require("../model/orderModel");
 const sendEmail = require("../utils/sendMail");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 //create order
 exports.createOrder = catchAsyncError(async (req, res, next) => {
+  console.log(req.body);
   const { products } = req.body;
 
   // check product is available or not
@@ -55,10 +57,15 @@ exports.createOrder = catchAsyncError(async (req, res, next) => {
   //   message: "Your Order placed successfully",
   // });
 
-  res.status(200).json({
-    success: true,
-    message: "Order placed successfully!",
-  });
+  // res.status(200).json({
+  //   success: true,
+  //   message: "Order placed successfully!",
+  //   products: products,
+  //   // clientSecret:
+  //   //   "pk_test_51SQi6rQLy61FKKAvt7EIpsakkyIRp2oDs2mIOgdzrwpZLfOhJkcVvWkhg1XQpBgEUjE9PrugkTtxxpeOlr8lCw0K00nkQJIm1g",
+  // });
+
+  next();
 });
 
 // cancel order
@@ -70,5 +77,36 @@ exports.cancelOrder = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Order cancelled successfully!",
+  });
+});
+
+// payment
+exports.paymentController = catchAsyncError(async (req, res, next) => {
+  const product = await stripe.products.create({
+    name: "Chirag",
+  });
+
+  const price = await stripe.prices.create({
+    product: product.id,
+    unit_amount: 100 * 100, // $100
+    currency: "usd",
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: price.id,
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000",
+    cancel_url: "http://localhost:3000/mycart",
+    customer_email: "chiragpadsalatt11@gmail.com",
+  });
+
+  res.status(200).json({
+    success: true,
+    session: session.url,
   });
 });
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { createOrder, cancelOrder } = require("../controller/orderController");
+const {
+  createOrder,
+  cancelOrder,
+  paymentController,
+} = require("../controller/orderController");
 const { isAuthenticatedUser } = require("../middleware/isAuthenticated");
 
 const router = express.Router();
 
-router.route("/create-order").post(isAuthenticatedUser, createOrder);
+router
+  .route("/create-order")
+  .post(isAuthenticatedUser, createOrder, paymentController);
 router.route("/cancel-order").post(isAuthenticatedUser, cancelOrder);
 router.route("/mycart").post(isAuthenticatedUser);
+// router.route("/payment").post(isAuthenticatedUser);
 
 module.exports = router;
